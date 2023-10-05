c29f6590dea455622a84ff9481a315cadcd68f64
Becca Stucki <44650994+rebeccaanns@users.noreply.github.com>
2023-10-05 10:43:14 -0600

    fonts and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      background: "F8F4F9",
+      red: "FB3640",
+      violet: "A36BAD",
+      purple: "6A62AC",
+      darkPurple: "49437E",
+      blue: "46AEE2",
+      green: "0A8754",
+    },
+    fontFamily: {
+      body: ["Quicksand", "sans-serif"],
+      head: ["Yeseva One", "cursive"],
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
