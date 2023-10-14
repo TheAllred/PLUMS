d1801baa08334d2372cba6267367ccb031a9f7ee
Kevin Allred <16504353+TheAllred@users.noreply.github.com>
2023-10-14 09:14:03 -0600

    add cards WIP

---
+++ src/app/cards/page.tsx
@@ -0,0 +1,208 @@
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+*/
+"use client";
+import { Fragment, useState } from 'react'
+import { Dialog, Disclosure, Menu, Popover, Tab, Transition } from '@headlessui/react'
+import { Bars3Icon, MagnifyingGlassIcon, ShoppingCartIcon, UserIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+const currencies = ['CAD', 'USD', 'AUD', 'EUR', 'GBP']
+const navigation = {
+  categories: [
+    {
+      name: 'Women',
+      featured: [
+        { name: 'Sleep', href: '#' },
+        { name: 'Swimwear', href: '#' },
+        { name: 'Underwear', href: '#' },
+      ],
+      collection: [
+        { name: 'Everything', href: '#' },
+        { name: 'Core', href: '#' },
+        { name: 'New Arrivals', href: '#' },
+        { name: 'Sale', href: '#' },
+      ],
+      categories: [
+        { name: 'Basic Tees', href: '#' },
+        { name: 'Artwork Tees', href: '#' },
+        { name: 'Bottoms', href: '#' },
+        { name: 'Underwear', href: '#' },
+        { name: 'Accessories', href: '#' },
+      ],
+      brands: [
+        { name: 'Full Nelson', href: '#' },
+        { name: 'My Way', href: '#' },
+        { name: 'Re-Arranged', href: '#' },
+        { name: 'Counterfeit', href: '#' },
+        { name: 'Significant Other', href: '#' },
+      ],
+    },
+    {
+      name: 'Men',
+      featured: [
+        { name: 'Casual', href: '#' },
+        { name: 'Boxers', href: '#' },
+        { name: 'Outdoor', href: '#' },
+      ],
+      collection: [
+        { name: 'Everything', href: '#' },
+        { name: 'Core', href: '#' },
+        { name: 'New Arrivals', href: '#' },
+        { name: 'Sale', href: '#' },
+      ],
+      categories: [
+        { name: 'Artwork Tees', href: '#' },
+        { name: 'Pants', href: '#' },
+        { name: 'Accessories', href: '#' },
+        { name: 'Boxers', href: '#' },
+        { name: 'Basic Tees', href: '#' },
+      ],
+      brands: [
+        { name: 'Significant Other', href: '#' },
+        { name: 'My Way', href: '#' },
+        { name: 'Counterfeit', href: '#' },
+        { name: 'Re-Arranged', href: '#' },
+        { name: 'Full Nelson', href: '#' },
+      ],
+    },
+  ],
+  pages: [
+    { name: 'Company', href: '#' },
+    { name: 'Stores', href: '#' },
+  ],
+}
+const breadcrumbs = [
+  { id: 1, name: 'Objects', href: '#' },
+  { id: 2, name: 'Workspace', href: '#' },
+  { id: 3, name: 'Sale', href: '#' },
+]
+const sortOptions = [
+  { name: 'Most Popular', href: '#', current: true },
+  { name: 'Best Rating', href: '#', current: false },
+  { name: 'Newest', href: '#', current: false },
+  { name: 'Price: Low to High', href: '#', current: false },
+  { name: 'Price: High to Low', href: '#', current: false },
+]
+const filters = [
+  {
+    id: 'category',
+    name: 'Category',
+    options: [
+      { value: 'new-arrivals', label: 'All New Arrivals', checked: false },
+      { value: 'tees', label: 'Tees', checked: false },
+      { value: 'objects', label: 'Objects', checked: true },
+      { value: 'sweatshirts', label: 'Sweatshirts', checked: false },
+      { value: 'pants-shorts', label: 'Pants & Shorts', checked: false },
+    ],
+  },
+  {
+    id: 'color',
+    name: 'Color',
+    options: [
+      { value: 'white', label: 'White', checked: false },
+      { value: 'beige', label: 'Beige', checked: false },
+      { value: 'blue', label: 'Blue', checked: false },
+      { value: 'brown', label: 'Brown', checked: false },
+      { value: 'green', label: 'Green', checked: false },
+      { value: 'purple', label: 'Purple', checked: false },
+    ],
+  },
+  {
+    id: 'sizes',
+    name: 'Sizes',
+    options: [
+      { value: 'xs', label: 'XS', checked: false },
+      { value: 's', label: 'S', checked: false },
+      { value: 'm', label: 'M', checked: false },
+      { value: 'l', label: 'L', checked: false },
+      { value: 'xl', label: 'XL', checked: false },
+      { value: '2xl', label: '2XL', checked: false },
+    ],
+  },
+]
+const activeFilters = [{ value: 'objects', label: 'Objects' }]
+const products = [
+  {
+    id: 1,
+    name: 'React cheat sheet',
+    href: '#',
+    price: '',
+    imageSrc: 'https://images.pexels.com/photos/317356/pexels-photo-317356.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    imageAlt: 'Sheet with example code',
+  },
+  {
+    id: 2,
+    name: 'Conference talk',
+    href: '#',
+    price: '',
+    imageSrc: 'https://images.pexels.com/photos/6949978/pexels-photo-6949978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    imageAlt: 'Person speaking at podium',
+  },
+  {
+    id: 3,
+    name: 'Class notes',
+    href: '#',
+    price: '',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+    imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+  },
+  {
+    id: 4,
+    name: 'Hobby list',
+    href: '#',
+    price: '',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+    imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+  },
+  // More products...
+]
+
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Example() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
+
+  return (
+    <section
+            aria-labelledby="products-heading"
+            className="mx-auto max-w-2xl px-4 pb-16 pt-12 sm:px-6 sm:pb-24 sm:pt-16 lg:max-w-7xl lg:px-8"
+          >
+            <h2 id="products-heading" className="sr-only">
+              Products
+            </h2>
+
+            <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+              {products.map((product) => (
+                <a key={product.id} href={product.href} className="group">
+                  <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                    <img
+                      src={product.imageSrc}
+                      alt={product.imageAlt}
+                      className="h-full w-full object-cover object-center group-hover:opacity-75"
+                    />
+                  </div>
+                  <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+                  <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+                </a>
+              ))}
+            </div>
+          </section>
+  )
+}
