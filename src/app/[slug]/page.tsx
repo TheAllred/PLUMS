3da651840e021be176a4822717b6b79ea392441f
Kevin Allred <16504353+TheAllred@users.noreply.github.com>
2023-11-14 11:02:30 -0700

    Remove Supabase dependency and unused code

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,24 +1,14 @@
 "use client";
-import supabase from "../../db/db";
 import { useEffect, useState } from "react";
 import AddButton from "@/components/addButton";
 import Detail from "@/components/detail";
 export default function Page({ params }: { params: { slug: number } }) {
   const [data, setData] = useState([]);
   useEffect(() => {
-    const fetchNoteById = async () => {
-      let { data: note, error } = await supabase
-        .from("note")
-        .select("*")
-        .where("id", params.slug)
-        .limit(1);
+   
+  
 
-      if (error) console.log("error", error);
-      else setData(note);
-    };
-
-    fetchNoteById();
-  }, [supabase]);
+  });
 
   return (
     <>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,6 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { createContext, useContext, useEffect } from 'react';
 
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://gmmdogzicvjmjhsiqshx.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-
 export default function RootLayout({
   children,
 }: {
