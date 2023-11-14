8e95bedfa4caff4a822d762e41b207c947b1eda7
Kevin Allred <16504353+TheAllred@users.noreply.github.com>
2023-11-14 10:52:59 -0700

    sync with vercel/prisma

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,21 +1,20 @@
 "use client";
-import { Database } from "@/db/supabase";
-import supabase from "../../db/db";
 import { useEffect, useState } from "react";
 import Cards from "@/components/card";
+import { sql } from "@vercel/postgres";
+
+const likes = 100;
 
 export default function Page() {
   const [data, setData] = useState([]);
   useEffect(() => {
     const fetchTodos = async () => {
-      let { data: note, error } = await supabase.from("note").select("*");
-
-      if (error) console.log("error", error);
-      else setData(note);
+      // let { data: note, error } = await supabase.from("note").select("*");
+      // if (error) console.log("error", error);
+      // else setData(note);
+      const { rows } = await sql`SELECT * FROM posts WHERE likes > ${likes};`;
     };
-
-    fetchTodos();
-  }, [supabase]);
+  });
 
   return (
     <div>
@@ -23,3 +22,5 @@ export default function Page() {
     </div>
   );
 }
+
+
--- src/db/db.tsx
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://gmmdogzicvjmjhsiqshx.supabase.co";
-// const supabaseKey = process.env.SUPABASE_KEY;
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdtbWRvZ3ppY3ZqbWpoc2lxc2h4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5ODE2NTA0MCwiZXhwIjoyMDEzNzQxMDQwfQ.F19V3vU0E6r_bLn0c-X4mfLV2QvKklQsgAvlBMOAeQY";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
