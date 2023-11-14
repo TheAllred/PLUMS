0f3952ac2b6c9fb1e1be0529f8bdb1ee306e7312
Becca Stucki <44650994+rebeccaanns@users.noreply.github.com>
2023-11-14 11:02:52 -0700

    delete :(

--- src/components/settingsForm.tsx
+++ src/components/settingsForm.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState } from "react";
-import Switcher from "./darkSwitcher";
 
 function SettingsForm() {
   // const [theme, setTheme] = useState("light");
@@ -38,7 +37,6 @@ function SettingsForm() {
   return (
     <div className="font-body bg-white dark:bg-black">
       <h3 className="text-black dark:text-white">Theme</h3>
-      <Switcher />
       {/* <select value={theme} onChange={handleThemeChange}>
         <option value="light">Light</option>
         <option value="dark">Dark</option>
